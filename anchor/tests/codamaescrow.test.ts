e30b890589a4ca92a9b07420492b2434a8dcc439
rohaan-sahu <rbbsr8292@gmail.com>
2025-11-30 09:18:42 +0000

    adding program deployment test

--- anchor/tests/codamaescrow.test.ts
+++ anchor/tests/codamaescrow.test.ts
@@ -1,7 +1,7 @@
 import { before, describe, test, it } from "node:test";
 import assert, { doesNotMatch } from "node:assert";
 import * as programClient from "../dist/js-client";
-import { getOfferDecoder, OFFER_DISCRIMINATOR } from "../dist/js-client";
+import { getOfferDecoder, OFFER_DISCRIMINATOR,CODAMAESCROW_PROGRAM_ADDRESS } from "../dist/js-client";
 import { connect, Connection, TOKEN_EXTENSIONS_PROGRAM, ErrorWithTransaction } from "solana-kite";
 import { type KeyPairSigner, type Address } from "@solana/kit";
 import { createTestOffer, getRandomBigInt, ONE_SOL } from "./codamaescrow.test-helpers.ts";
@@ -13,9 +13,10 @@ describe("Escrow", async () => {
   let user: KeyPairSigner;
   let alice: KeyPairSigner;
   let bob: KeyPairSigner;
+  let program = CODAMAESCROW_PROGRAM_ADDRESS;
 
   before(async () => {
-    connection = await connect();
+    connection = connect();
 
     // 'user' will be the account we use to create the token mints
     [user, alice, bob] = await connection.createWallets(3, { airdropAmount: ONE_SOL });
@@ -28,9 +29,28 @@ describe("Escrow", async () => {
       const bobBalance = await connection.getLamportBalance(bob.address);
       const userBalance = await connection.getLamportBalance(user.address);
 
-      assert.equal(aliceBalance,ONE_SOL,"Alice wallet exists and has 1 SOL in funds");
-      assert.equal(bobBalance,ONE_SOL,"Bob wallet exists and has 1 SOL in funds");
-      assert.equal(userBalance,ONE_SOL,"User wallet exists and has 1 SOL in funds");
+      assert.equal(aliceBalance,ONE_SOL,"Alice's Wallet not funded as expected");
+      assert.equal(bobBalance,ONE_SOL,"Bob's wallet not funded as expected");
+      assert.equal(userBalance,ONE_SOL,"User's wallet not funded as expected");
+
+      console.log(`alice wallet balance: ${aliceBalance}`);
+      console.log(`bob wallet balance: ${bobBalance}`);
+      console.log(`user wallet balance: ${userBalance}`);
     })
+
+    
+    test("program is deployed", async () => {
+      const programId = CODAMAESCROW_PROGRAM_ADDRESS;
+      
+      const programAccountBal = await connection.getLamportBalance(programId);
+      console.log(`Program account balance: ${programAccountBal}`);
+
+      assert.notEqual(
+        programAccountBal,
+        0,
+        "The Program account has zero SOL. It may not have been deployed"
+      );
+    });
+    
   });
 });
